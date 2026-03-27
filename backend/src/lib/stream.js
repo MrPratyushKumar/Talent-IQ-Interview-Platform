87fa66d0dd1fd6e12beea9bed426b20ab2962883
Pratyush Pandey <pratyushkr4148@gmail.com>
2026-03-27 10:18:35 +0530

    feat: auth and stream sync

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,34 @@
+import { StreamChat } from "stream-chat"
+import {ENV} from "./env.js"
+
+const apiKey =  ENV.STREAM_API_KEY
+const apiSecret =  ENV.STREAM_API_SECRET
+
+if(!apiKey || !apiSecret){
+  console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
+}
+
+export const chatClient = StreamChat.getInstance(apiKey , apiSecret);// its means we can interact with the stream
+
+// upsert means both create and update the data
+export const upsertStreamUser = async(userData) => {
+  try {
+    await chatClient.upsertUsers(userData)
+    console.log("Stream user upserting successfully:", userData) 
+  } catch (error) {
+    console.log("Error upserting Stream user",error);
+  }
+}
+
+//
+export const deleteStreamUser = async(userId) => {
+  try {
+    await chatClient.deleteUser(userId);
+    console.log("Stream user deleted successfully:", userId)
+  } catch (error) {
+    console.error("Error deleting the Stream user:", error);
+  }
+}
+
+
+// todo add another method to generateToken
